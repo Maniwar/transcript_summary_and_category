82ad821002de5a0927b6534cb877224aa5453f08
Maniwar <mberenji@gmail.com>
2023-07-12 22:54:49 -0700

    Update transcript_csv.py

--- transcript_csv.py
+++ transcript_csv.py
@@ -273,7 +273,7 @@ if transcript_file is not None:
     new_category_name = st.sidebar.text_input("New Agent Category Name")
     new_category_subcategories = st.sidebar.text_area(f"Subcategories for Agent Category {new_category_name}")
     if new_category_name and new_category_subcategories:
-        agent_categories_edited[new_category_name] = category_subcategories.split("\n")
+        agent_categories_edited[new_category_name] = new_category_subcategories.split("\n")
 
     # Main processing
     if start_processing:
@@ -360,17 +360,3 @@ if transcript_file is not None:
         b64 = base64.b64encode(csv_data.encode()).decode()
         href = f'<a href="data:file/csv;base64,{b64}" download="processed_transcripts.csv">Download CSV</a>'
         st.markdown(href, unsafe_allow_html=True)
-
-ValueError: not enough values to unpack (expected 2, got 1)
-Traceback:
-File "C:\Python311\Lib\site-packages\streamlit\runtime\scriptrunner\script_runner.py", line 552, in _run_script
-    exec(code, module.__dict__)
-File "C:\Users\m.berenji\Desktop\To Move\git\NPS Script\categorizer\transcript_category_csv.py", line 325, in <module>
-    best_intent = max(intent_scores, key=lambda x: max([score for _, score in x[1]]))
-                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-File "C:\Users\m.berenji\Desktop\To Move\git\NPS Script\categorizer\transcript_category_csv.py", line 325, in <lambda>
-    best_intent = max(intent_scores, key=lambda x: max([score for _, score in x[1]]))
-                                                       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-File "C:\Users\m.berenji\Desktop\To Move\git\NPS Script\categorizer\transcript_category_csv.py", line 325, in <listcomp>
-    best_intent = max(intent_scores, key=lambda x: max([score for _, score in x[1]]))
-                                                                  ^^^^^^^^
