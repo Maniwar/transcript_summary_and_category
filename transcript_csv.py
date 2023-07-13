68a23df1aabdd3ac841948e71b79a29875db4732
Maniwar <mberenji@gmail.com>
2023-07-12 21:23:24 -0700

    Update transcript_csv.py

--- transcript_csv.py
+++ transcript_csv.py
@@ -4,7 +4,7 @@ import base64
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 from transformers import T5ForConditionalGeneration, T5Tokenizer
-import chartdet
+import chardet
 
 # Initialize BERT model
 @st.cache_resource  # Cache the BERT model as a resource
