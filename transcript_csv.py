3f618c67586ee4f18918b0935916bb8a20ad5af6
Maniwar <mberenji@gmail.com>
2023-07-12 23:49:36 -0700

    Update transcript_csv.py

--- transcript_csv.py
+++ transcript_csv.py
@@ -231,3 +231,27 @@ if transcript_file is not None:
         b64 = base64.b64encode(csv_data.encode()).decode()
         href = f'<a href="data:file/csv;base64,{b64}" download="processed_transcripts.csv">Download CSV</a>'
         st.markdown(href, unsafe_allow_html=True)
+
+ValueError: Must have equal len keys and value when setting with an iterable
+Traceback:
+File "C:\Python311\Lib\site-packages\streamlit\runtime\scriptrunner\script_runner.py", line 552, in _run_script
+    exec(code, module.__dict__)
+File "C:\Users\m.berenji\Desktop\To Move\git\NPS Script\categorizer\transcript_category_csv.py", line 217, in <module>
+    df.at[batch_start:batch_end, "Best Matching Customer Category"] = best_customer_categories
+    ~~~~~^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+File "C:\Python311\Lib\site-packages\pandas\core\indexing.py", line 2442, in __setitem__
+    return super().__setitem__(key, value)
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+File "C:\Python311\Lib\site-packages\pandas\core\indexing.py", line 2397, in __setitem__
+    self.obj._set_value(*key, value=value, takeable=self._takeable)
+File "C:\Python311\Lib\site-packages\pandas\core\frame.py", line 4221, in _set_value
+    self.loc[index, col] = value
+    ~~~~~~~~^^^^^^^^^^^^
+File "C:\Python311\Lib\site-packages\pandas\core\indexing.py", line 818, in __setitem__
+    iloc._setitem_with_indexer(indexer, value, self.name)
+File "C:\Python311\Lib\site-packages\pandas\core\indexing.py", line 1750, in _setitem_with_indexer
+    self._setitem_with_indexer(new_indexer, value, name)
+File "C:\Python311\Lib\site-packages\pandas\core\indexing.py", line 1795, in _setitem_with_indexer
+    self._setitem_with_indexer_split_path(indexer, value, name)
+File "C:\Python311\Lib\site-packages\pandas\core\indexing.py", line 1850, in _setitem_with_indexer_split_path
+    raise ValueError(
