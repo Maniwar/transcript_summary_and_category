9f18d9be1515986fcb4fab496faffff932918e79
Maniwar <mberenji@gmail.com>
2023-07-28 16:07:44 -0700

    Update batch_summary_v1.py

--- batch_summary_v1.py
+++ batch_summary_v1.py
@@ -99,7 +99,7 @@ def summarize_text(texts, max_length=100, min_length=50, max_tokens=1024, max_ch
     start_time = time.time()
     print("Start Summarizing text...")
     # Initialize the summarization pipeline
-    summarization_pipeline = pipeline("summarization")
+    summarization_pipeline = get_summarization_pipeline()
 
     # Initialize a list to store the summaries
     all_summaries = []
