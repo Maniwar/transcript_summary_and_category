0118f0a2b2f5ce241e9e2a6d66b7a7663476c403
Maniwar <mberenji@gmail.com>
2023-10-06 17:02:23 -0700

    Update category_tom.py

--- category_tom.py
+++ category_tom.py
@@ -1,75 +1,165 @@
 default_categories = {
-
    "Website Design & Navigation": {
-       "Issue1": [""],
+       "Issue1": [
+            "I find the website layout confusing.",
+            "The site design is not very intuitive.",
+            "Navigating through the website is a challenge."
+        ],
     },
    "Product Information": {
-       "Issue1": [""],
+       "Issue1": [
+            "The product details are not very clear.",
+            "I wish there was more information about this item.",
+            "The specifications for the product are missing."
+        ],
     },
    "Online Purchasing": {
-       "Issue1": [""],
+       "Issue1": [
+            "I had trouble completing my online purchase.",
+            "The checkout process on the website is complicated.",
+            "I couldn't find where to enter my payment details."
+        ],
     },
    "Delivery & Installation": {
-       "(DA) Cancelled" : [""],
-       "(DA) Carrier Feedback" : [""],
-       "(DA) Damages" : [""],
-       "(DA) Empty Box" : [""],
-       "(DA) Incorrect Delivery Dates - Post" : [""],
-       "(DA) Installation Issues" : [""],
-       "(DA) Lack of Delivery Delay Notification" : [""],
-       "(DA) Lack of Delivery Service" : [""],
-       "(DA) Lack of Delivery Service AGS" : [""],
-       "(DA) Lack of Delivery Service FedEx" : [""],
-       "(DA) Lack of Delivery Service Forward Air" : [""],
-       "(DA) Lack of Delivery Service RXO" : [""],
-       "(DA) Lack of Delivery Service UPS" : [""],
-       "(DA) No Delivery" : [""],
-       "(DA) Reschedules - No Cx notification" : [""],
-       "(DA) Tracking Issues" : [""],
-       "(DA) Where's My Stuff - Not Agent Related (Not BOPIS)" : [""],
-       "(DA) Wrong Address" : [""],
-       "(DA) Wrong Product Received" : [""],
-       "(VD) Cancelled" : [""],
-       "(VD) Carrier Feedback" : [""],
-       "(VD) Damages" : [""],
-       "(VD) Empty Box" : [""],
-       "(VD) Incorrect Delivery Dates - Post" : [""],
-       "(VD) Installation Issues" : [""],
-       "(VD) Lack of Delivery Delay Notification" : [""],
-       "(VD) Lack of Delivery Service" : [""],
-       "(VD) Lack of Delivery Service AGS" : [""],
-       "(VD) Lack of Delivery Service FedEx" : [""],
-       "(VD) Lack of Delivery Service Forward Air" : [""],
-       "(VD) Lack of Delivery Service RXO" : [""],
-       "(VD) Lack of Delivery Service UPS" : [""],
-       "(VD) No Delivery" : [""],
-       "(VD) Reschedules - No CX notification" : [""],
-       "(VD) Tracking Issues" : [""],
-       "(VD) Where's My Stuff - Not Agent Related (Not BOPIS)" : [""],
-       "(VD) Wrong Address" : [""],
-       "(VD) Wrong Product Received" : [""],
-       "(MX) Cancelled" : [""],
-       "(MX) Carrier Feedback" : [""],
-       "(MX) Damages" : [""],
-       "(MX) Empty Box" : [""],
-       "(MX) Incorrect Delivery Dates - Post" : [""],
-       "(MX) Lack of Delivery Delay Notification" : [""],
-       "(MX) Lack of Delivery Service" : [""],
-       "(MX) Lack of Delivery Service AGS" : [""],
-       "(MX) Lack of Delivery Service FedEx" : [""],
-       "(MX) Lack of Delivery Service Forward Air" : [""],
-       "(MX) Lack of Delivery Service RXO" : [""],
-       "(MX) Lack of Delivery Service UPS" : [""],
-       "(MX) No Delivery" : [""],
-       "(MX) Tracking Issues" : [""],
-       "(MX) Where's My Stuff - Not Agent Related (Not BOPIS)" : [""],
-       "(MX) Wrong Address" : [""],
-       "(MX) Wrong Product Received" : [""],
+       "(DA) Cancelled": [
+            "My delivery was abruptly cancelled.",
+            "Why was my scheduled delivery called off?",
+            "I didn't receive any notice about the delivery cancellation."
+        ],
+       "(DA) Carrier Feedback": [
+            "The delivery personnel were very unprofessional.",
+            "I had a poor experience with the delivery service.",
+            "The delivery team did not handle my items carefully."
+        ],
+       "(DA) Damages": [
+            "My item arrived damaged.",
+            "The packaging was torn and the product was broken.",
+            "There were scratches and dents on my delivered product."
+        ],
+       "(DA) Empty Box": [
+            "I received an empty box instead of my product.",
+            "The box was sealed, but there was nothing inside.",
+            "My package came empty, where's my order?"
+        ],
+       "(DA) Incorrect Delivery Dates - Post": [
+            "The delivery date mentioned was wrong.",
+            "I was expecting the delivery today, but the date changed.",
+            "Why does the delivery date keep shifting?"
+        ],
+       "(DA) Installation Issues": [
+            "The installation of my home appliance was not up to par.",
+            "My installation of the home appliance was not done properly.",
+            "The technicians didn't install the appliance correctly."
+        ],
+       "(DA) Lack of Delivery Delay Notification": [
+            "I wasn't notified about the delivery delay.",
+            "There was no communication about the postponed delivery.",
+            "Why wasn't I informed about the change in delivery date?"
+        ],
+       "(DA) Lack of Delivery Service": [
+            "I was expecting delivery service but it wasn't provided.",
+            "There was no delivery service for my order.",
+            "Why wasn't there a delivery service for my purchase?"
+        ],
+       "(DA) Lack of Delivery Service AGS": [
+            "I had issues with the AGS delivery service.",
+            "The AGS delivery was not up to the mark.",
+            "AGS didn't deliver my product as expected."
+        ],
+       "(DA) Lack of Delivery Service FedEx": [
+            "FedEx didn't deliver on the promised date.",
+            "I had a poor experience with FedEx delivery.",
+            "Why was there a delay with FedEx's delivery?"
+        ],
+       "(DA) Lack of Delivery Service Forward Air": [
+            "Forward Air delivery was not smooth.",
+            "I faced issues with Forward Air's delivery service.",
+            "The delivery by Forward Air was not on time."
+        ],
+       "(DA) Lack of Delivery Service RXO": [
+            "RXO didn't provide a satisfactory delivery.",
+            "I wasn't happy with RXO's delivery service.",
+            "The delivery by RXO was delayed and problematic."
+        ],
+       "(DA) Lack of Delivery Service UPS": [
+            "UPS didn't deliver my package properly.",
+            "The delivery by UPS was delayed.",
+            "I had issues with UPS's delivery service."
+        ],
+       "(DA) No Delivery": [
+            "I haven't received my order yet.",
+            "My package never arrived.",
+            "Why hasn't my order been delivered yet?"
+        ],
+       "(DA) Reschedules - No Cx notification": [
+            "My delivery was rescheduled without notifying me.",
+            "I wasn't informed about the delivery rescheduling.",
+            "Why was my delivery date changed without notice?"
+        ],
+       "(DA) Tracking Issues": [
+            "I can't track my order on the website.",
+            "The tracking number provided isn't working.",
+            "Why isn't the tracking system updating the status of my delivery?"
+        ],
+       "(DA) Where's My Stuff - Not AGS": [
+            "Where is my order? I didn't use AGS for delivery.",
+            "I can't find any update on my package not delivered by AGS.",
+            "I haven't received my product and it wasn't shipped through AGS."
+        ],
+       "(DA) Where's My Stuff - Not FedEx": [
+            "I'm waiting for my package that's not delivered by FedEx.",
+            "Where's my order? FedEx isn't the carrier.",
+            "I can't track my order, and it wasn't shipped with FedEx."
+        ],
+       "(DA) Where's My Stuff - Not Forward Air": [
+            "I haven't received any update on my order not delivered by Forward Air.",
+            "Where is my product? It's not with Forward Air.",
+            "Can't find my package and it wasn't through Forward Air."
+        ],
+       "(DA) Where's My Stuff - Not RXO": [
+            "Where's my order? RXO isn't handling the delivery.",
+            "I'm still waiting for my package, and it's not with RXO.",
+            "I can't get an update on my shipment not carried by RXO."
+        ],
+       "(DA) Where's My Stuff - Not UPS": [
+            "I haven't received my order, and it wasn't shipped by UPS.",
+            "Where is my delivery? It's not with UPS.",
+            "I'm trying to track my package not delivered by UPS."
+        ],
+       "(DA) Where's My Stuff - Not Aggregated": [
+            "I can't find any information about my order.",
+            "Where is my order? I haven't received any updates.",
+            "I placed an order but can't see any details about it."
+        ],
+       "(DA) Where's My Stuff - Aggregated": [
+            "I'm having trouble tracking all my items together.",
+            "Can I see all my order details in one place?",
+            "How do I view aggregated information for my purchases?"
+        ],
+       "(DA) Delivery Delay - Pre": [
+            "Why is my delivery taking longer than expected?",
+            "The delivery date was pushed back before it even started.",
+            "I was told my item would arrive sooner."
+        ],
+       "(DA) Delivery Delay - Post": [
+            "My delivery date was changed after the initial estimate.",
+            "The delivery is taking longer than what was mentioned.",
+            "Why was my delivery date moved to a later day?"
+        ],
+       "(DA) Product Feedback": [
+            "I'm not satisfied with the product I received.",
+            "The item doesn't match its description.",
+            "I have some feedback about the product I got."
+        ],
     },
    "eCom Support": {
-       "Issue1": [""],
+       "Issue1": [
+            ""
+        ],
     },
    "CARE Support": {
-       "Issue1": [""],
+       "Issue1": [
+            ""
+        ],
     },
 }
